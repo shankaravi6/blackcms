206c214db7034cf3c494cb208d480415b032c43b
Shankar <69907363+shankaravi6@users.noreply.github.com>
2025-04-15 23:48:18 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -67,7 +67,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-
+app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
